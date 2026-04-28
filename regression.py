e539e9c4d0d3f96d09a0f0065662576c1634618a
ota hiraki <e245733@ie.u-ryukyu.ac.jp>
2026-04-28 10:32:11 +0900

    add regression

---
+++ regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self):
+        self.theta = None
+
+    def fit(self, X, Y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def score(self, X, Y):
+        pass
